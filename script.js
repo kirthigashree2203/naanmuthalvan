d0b7887315e28096e0a65abc9b5e1bcdd47b6853
kirthigashree2203 <kirthigashree562@gmail.com>
2024-05-28 13:25:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+    
+}
